c4541cb82c4197623bac2cee124a7f1cdc3ba061
Vaish181204 <144530714+Vaish181204@users.noreply.github.com>
2026-03-17 20:44:05 +0530

    Add files via upload

---
+++ pp1.py
@@ -0,0 +1,21 @@
+#area of a circle
+radius = float(input("Enter the radius of the circle: "))
+area = 3.14 * radius ** 2
+print("The area of the circle is:", area)
+
+#area of a rectangle
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+area_rectangle = length * width
+print("The area of the rectangle is:", area_rectangle)
+
+#area of a triangle
+base = float(input("Enter the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+area_triangle = 0.5 * base * height
+print("The area of the triangle is:", area_triangle)
+
+#area of a square
+side = float(input("Enter the side length of the square: "))
+area_square = side ** 2
+print("The area of the square is:", area_square)
